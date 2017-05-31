e6d6ce6e1b56b683d4a3d8aac4c21ac70bca1fd7
David Kouřil <david.kouril@gmail.com>
2017-05-31 06:22:34 +0200

    Add gitignore for Xcode and Swift

---
+++ when-to-work/when-to-work/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  when-to-work
+//
+//  Created by David Kouril on 31/05/2017.
+//  Copyright © 2017 David Kouřil. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ when-to-work/when-to-work/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  when-to-work
+//
+//  Created by David Kouril on 31/05/2017.
+//  Copyright © 2017 David Kouřil. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
