40edf88ea13695a8a22515787c12d51cc01dc4ac
David Kouřil <david.kouril@gmail.com>
2017-06-01 07:54:57 +0200

    Add DayBar file

---
+++ when-to-work/when-to-work/DayBar.swift
@@ -0,0 +1,15 @@
+//
+//  DayBar.swift
+//  when-to-work
+//
+//  Created by David Kouril on 01/06/2017.
+//  Copyright © 2017 David Kouřil. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import Alamofire
+
+class DayBar {
+    
+}
